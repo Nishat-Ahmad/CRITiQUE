2912019ab7cdf8f48ed7fd7934667199ded37938
Nishat Ahmad <nishat.ahmed1200@gmail.com>
2025-12-19 20:46:07 +0500

    Removed old main.py and the redundant db file

--- app.py
+++ app.py
@@ -552,4 +552,4 @@ async def add_dish_post(request: Request, place_id: int, name: str = Form(...), 
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=True)
+    uvicorn.run("app:app", host='127.0.0.1', port=8000, reload=True)
